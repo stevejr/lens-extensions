3c4678cfdffb0aa86e1a74c7f0cf5c080f2ed53e
Steve Richards <srichards@mirantis.com>
2021-08-05 10:33:43 +0100

    Add link to SourceRef from main HelmChart page

--- fluxv2/src/pages/helmcharts.tsx
+++ fluxv2/src/pages/helmcharts.tsx
@@ -1,7 +1,11 @@
-import { Renderer } from "@k8slens/extensions";
+import { Common, Renderer } from "@k8slens/extensions";
 import React from "react";
+import { Link } from "react-router-dom";
 import { HelmChart } from "../source-controller/helmchart";
 import { helmChartStore } from "../source-controller/helmchart-store";
+import { bucketStore } from "../source-controller/bucket-store";
+import { gitRepositoryStore } from "../source-controller/gitrepository-store";
+import { helmRepositoryStore } from "../source-controller/helmrepository-store";
 
 import "./helmcharts.scss";
 
@@ -14,9 +18,56 @@ const enum sortBy {
 
 }
 
+const {
+  Component: {
+    Badge,
+  },
+  Navigation: {
+    getDetailsUrl
+  }
+} = Renderer;
+
+const { stopPropagation } = Common.Util;
+
 export class HelmChartsPage extends React.Component<{ extension: Renderer.LensExtension }> {
   getHelmChartSource(helmChart: HelmChart) {
     return <>Kind: {helmChart.spec.sourceRef.kind}<br />Name: {helmChart.spec.sourceRef.name}</>;
+  }
+
+  async componentDidMount() {
+    await bucketStore.loadAll();
+    await gitRepositoryStore.loadAll();
+    await helmRepositoryStore.loadAll();
+  }
+
+  getSourceRefLink(helmChart: HelmChart) {
+    const { kind, name } = helmChart.spec.sourceRef;
+
+    switch (kind) {
+      case "Bucket":
+        const bucket = bucketStore.getByName(name);
+
+        return bucket.selfLink;
+      case "GitRepository":
+        const gitRepo = gitRepositoryStore.getByName(name);
+
+        return gitRepo.selfLink;
+
+      case "HelmRepository":
+        const helmRepo = helmRepositoryStore.getByName(name);
+
+        return helmRepo.selfLink;
+    } 
+  }
+
+  getSource(helmChart: HelmChart) {
+    const selfLink = this.getSourceRefLink(helmChart);
+
+    return <><Badge flat key={helmChart.spec.sourceRef.kind} className="sourceRef" tooltip={helmChart.spec.sourceRef.name} expandable={false} onClick={stopPropagation}>
+      <Link to={getDetailsUrl(selfLink)}>
+        {helmChart.spec.sourceRef.kind}
+      </Link>
+    </Badge></>;
   }
 
   render() {
@@ -41,14 +92,14 @@ export class HelmChartsPage extends React.Component<{ extension: Renderer.LensEx
         renderTableContents={(helmChart: HelmChart) => [
           helmChart.getName(),
           helmChart.metadata.namespace,
-          this.getHelmChartSource(helmChart),
+          this.getSource(helmChart),
           helmChart.spec?.version ?? ""
         ]}
-        tableProps={{
-          customRowHeights: (item: HelmChart, lineHeight, paddings) => {
-            return 2 * lineHeight + paddings;
-          }
-        }}
+        // tableProps={{
+        //   customRowHeights: (item: HelmChart, lineHeight, paddings) => {
+        //     return 2 * lineHeight + paddings;
+        //   }
+        // }}
       />
     );
   }
